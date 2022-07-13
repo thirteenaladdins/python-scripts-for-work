4235d8361ab88f795ffadd5705b30861d591bd2b
thirteenaladdins <moamrr@gmail.com>
2022-07-13 12:59:57 +0100

    new file

---
+++ rename_files.py
@@ -0,0 +1,17 @@
+# python standard library
+# import os 
+# also check for multiple files 
+ 
+# read folder name from file path
+# get all files in directory - put file names into list - or tuple?
+# get file name from newly added file
+# compare file names in list of files
+
+# if exists - add - 001 to file 
+# check 
+# if 001 exists add 002
+
+# check file name - 
+
+
+
