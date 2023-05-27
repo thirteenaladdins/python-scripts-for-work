b041a5ee41bea42958bd2dac8d338e6923a7b40e
thirteenaladdins <moamrr@gmail.com>
2023-05-27 18:46:59 +0100

    refactor(hb_fuller_preprocessor.py): remove unused imports and commented out code feat(hb_fuller_preprocessor.py): add support for selecting sheet, minimum number of non-null values in a row, reference, customer, week number, and incoterms using InquirerPy docs(hb_fuller_preprocessor.py): add TODOs for future improvements such as summary printing and notifying user if columns don't exist
    refactor(hb_fuller_preprocessor.py): remove commented out code and unused imports

--- cct_processing/hb_fuller_preprocessor.py
+++ cct_processing/hb_fuller_preprocessor.py
@@ -1,6 +1,231 @@
-from PyInquirer import prompt
+# from PyInquirer import prompt
+# from prompt_toolkit import PromptSession
+# from prompt_toolkit.completion import WordCompleter
+# import prompt_engine
+# from prompt_toolkit.shortcuts import checkboxlist_dialog
+# from prompt_toolkit import PromptSession
+# from prompt_toolkit.completion import WordCompleter
+# from prompt_toolkit.validation import Validator
+
+from InquirerPy import prompt, inquirer
+from InquirerPy.utils import color_print
 import pandas as pd
 import sys
+
+# TODO: come back and amend this
+# this will just have to match the 912 Reference, to be added as the importer
+# setup in CCT
+HBF_CUSTOMERS = [
+    'HBF912UK (912)',
+    'HBF914UK (914)'
+]
+
+
+additional_header_data = {
+    "interfaceVersion": "4.2",  # no additions needed # mandatory
+    "customerIdCCT": "",
+    "customerEmail": "mamer@als-cs.com",  # no additions needed # mandatory
+    "customerReference": '',  # get this from the CLI
+    "deliveryTerm_SAD20": "",  # fetch this data from the worksheet after selection is made
+    # fetch this data from the worksheet - we can only pick one
+    "deliveryTermPlace_SAD20": "",
+    "countryOfExport_SAD15": "",  # pick the first?
+    "countryOfDestination_SAD17": "",  # pick the first?
+    "totalPackages_SAD06": "",  # sum of quantity in each line - sum this after the data
+    "purchaseCountry_SAD11": "",  # pick first? Mandatory field
+    "totalAmountInvoiced_SAD22": "",
+    # highlight if there are multiple currencies in worksheet
+    "totalAmountInvoicedCurrency_SAD22": ""
+    # "totalGrossMass": 2914.0,  # sum of weight column - optional
+
+}
+
+
+selected_columns = ["Plant Name", "Sales Order Nbr", "Incoterms", "10-Digit UK Import", "Number Pieces",
+                    "Qty Shipped Net Weight KG", "Customs Value", "Currency", "Country Of Origin",
+                    "General description", "Date Creation Record", "EORI Nbr"]
+
+address_data_table = {
+    "Blois": ["FRHBFBLO01", "HB FULLER", "ALLEE ROBERT SCHUMAN C.S 1308", "BLOIS", "41013", "FR"],
+    "Lueneburg": ["DEHBFLUE01", "H.B. Fuller Deutschland GmbH", "An der Roten Bleiche 2-3", "Lueneburg", "21335", "DE"],
+    "Pianezze": ["ITHBFPIA01", "HB Fuller", "VIA DEL INDUSTRIA 8", "PIANEZZE", "36060", "IT"],
+    "Nienburg": ["DEHBFNIE01", "HB FULLER DEUTSCHLAND PRODUKTIONS", "HENRIETTENSTRASSE 32", "NIENBURG", "31582", "DE"],
+    "Mindelo": ["PTHBFMIN01", "HB FULLER", "Estrada Nacional 13/km16", "Mindelo", "4486-851", "PT"],
+}
+
+
+def get_worksheets(input_file):
+    excel_file = pd.ExcelFile(input_file)
+    return excel_file.sheet_names
+
+
+def read_excel(file_path, sheet_name=None):
+    df = pd.read_excel(file_path, sheet_name=sheet_name)
+    return df
+
+
+def process_excel(df):
+    df.insert(0, 'sequentialNo_SAD32', range(1, len(df) + 1))
+    df.rename(columns={"Currency": "goodsValueCurrency",
+                       "General description": "goodsDescription_SAD31",
+                       "Qty Shipped Net Weight KG": "netMass_SAD38",
+                       "Number Pieces": "noOfPackages_SAD31",
+                       "10-Digit UK Import": "customerHSCode_SAD33im",
+                       "Customs Value": "itemPrice_SAD42",
+                       "Country Of Origin": "countryOfOrigin_SAD34",
+                       "Sales Order Nbr": "invoiceNo"}, inplace=True)
+    df['grossMass_SAD35'] = df['netMass_SAD38']
+    return df
+
+
+def add_address_data(row):
+    address_rows = {}
+    plant_names = ["Blois", "Lueneburg", "Pianezze", "Nienburg", "Mindelo"]
+    for name in plant_names:
+        if name in row["Plant Name"]:
+            data = address_data_table[name]
+            address_rows = {
+                "Exporter Name": data[1],
+                "Exporter Street":  data[2],
+                "Exporter City": data[3],
+                "Exporter PostCode": data[4],
+                "Exporter Country": data[5],
+                "Exporter ShortCode": data[0],
+                "Dispatch Country": data[5],
+                "Destination Country": 'GB',
+            }
+    return address_rows
+
+
+def convert_df_to_json(df):
+    df.to_json('output.json', orient='records', lines=True)
+
+
+# change colours
+
+def main(input_file):
+    sheets = get_worksheets(input_file)
+    # excel_file = pd.ExcelFile()
+
+    # df = read_excel(input_file, sheets[])
+    # print(df.columns)
+
+    sheet = inquirer.select(
+        message="Select a sheet:",
+        choices=sheets
+    ).execute()
+
+    valid_row_threshold = inquirer.text(
+        message="Enter the minimum number of non-null values in a row:",
+        validate=lambda val: val.isdigit() and int(val) > 0,
+        transformer=lambda val: int(val)
+    ).execute()
+
+    user_input = inquirer.text(
+        message='Please enter Reference [box 7]:'
+    ).execute()
+
+    customer_list = inquirer.select(
+        message='Select a customer',
+        choices=HBF_CUSTOMERS
+    ).execute()
+
+    df = read_excel(input_file, sheet)
+    df_remove_null = df.dropna(thresh=int(valid_row_threshold))
+
+    additional_header_data['customerReference'] = user_input
+    additional_header_data['customerIdCCT'] = customer_list
+
+    week_list = df['Calendar Week'].unique().tolist()
+
+    week_list = [str(week_number)
+                 for week_number in week_list if str(week_number) != 'nan']
+
+    week_number = inquirer.select(
+        message='Select a week',
+        choices=week_list
+    ).execute()
+
+    incoterms = df['Incoterms'].unique().tolist()
+    incoterms = [str(incoterm)
+                 for incoterm in incoterms if str(incoterm) != 'nan']
+
+    # this is quite confusing but it works.
+    while True:
+        selected_incoterms = []
+
+        while True:
+            available_incoterms = [
+                incoterm for incoterm in incoterms if incoterm not in selected_incoterms]
+
+            # so we break the loop if there are no incoterms to select
+            if not available_incoterms:  # All incoterms have been selected
+                break
+
+            # here is where we need to break
+            answers_2 = inquirer.checkbox(
+                message="Select incoterms (use Space to select, Enter to confirm) " +
+                (f' (selected incoterms: {selected_incoterms})' if selected_incoterms else ''),
+                choices=[{'name': incoterm, 'value': incoterm}
+                         for incoterm in available_incoterms]
+            ).execute()
+
+            if not answers_2:  # If the user didn't select any new incoterm, we move on to the confirmation step
+                color_print(formatted_text=[("class:red", "Please select a term ")], style={
+                            "red": "red", })
+            else:
+                for incoterm in answers_2:
+                    if incoterm not in selected_incoterms:
+                        selected_incoterms.append(incoterm)
+
+                break
+
+        done = inquirer.confirm(
+            f"You've selected these incoterms: {selected_incoterms}. Do you want to proceed?",
+        ).execute()
+
+        # TODO: come back and add formatting style for all
+        if done:
+            # proceed with the rest of your code
+            print("You selected the following settings:")
+            print(f'Selected sheet: {sheet}')
+            print(
+                f"Minimum number of non-null values in a row: {valid_row_threshold}")
+            print(f'Reference [box 7]: {user_input}')
+            print(f'Week Number: {week_number}')
+            print(f'Incoterms: {selected_incoterms}')
+
+            inquirer.confirm(
+                f"Do you want to proceed?",
+            ).execute()
+
+            break
+
+        else:
+            # If 'No' was selected, the outer loop will restart the selection process
+            # is there anythign to do here?
+            pass
+
+    #
+    # df_subset = df_remove_null[selected_columns].copy()
+    # processed_df = process_excel(df_subset)
+
+    print(f"Succesfully processed json file")
+    # convert_df_to_json(processed_df)
+
+# allow the user to go through each of the options
+# and amend any mistakes.
+# TODO: print summary to user before confirmation>
+# I'll add this after the processing script is done. then
+# TODO: if the columns don't exist, then notify the user and then move through the options
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("format: python hb_fuller_preprocessor <excel workbook>")
+        sys.exit(1)
+
+    main(sys.argv[1])
 
 # this should be the interface, and then the hb fuller processor should be a separate file
 
@@ -113,294 +338,48 @@ import sys
 # the other thing is that we're assuming that someone is always going to use
 # the HBF preprocessor
 
-additional_header_data = {
-    "interfaceVersion": "4.2",  # no additions needed # mandatory
-    "customerIdCCT": "",
-    "customerEmail": "mamer@als-cs.com",  # no additions needed # mandatory
-    "customerReference": '',  # get this from the CLI
-    "deliveryTerm_SAD20": "",  # fetch this data from the worksheet after selection is made
-    # fetch this data from the worksheet - we can only pick one
-    "deliveryTermPlace_SAD20": "",
-    "countryOfExport_SAD15": "",  # pick the first?
-    "countryOfDestination_SAD17": "",  # pick the first?
-    "totalPackages_SAD06": "",  # sum of quantity in each line - sum this after the data
-    "purchaseCountry_SAD11": "",  # pick first? Mandatory field
-    "totalAmountInvoiced_SAD22": "",
-    # highlight if there are multiple currencies in worksheet
-    "totalAmountInvoicedCurrency_SAD22": ""
-    # "totalGrossMass": 2914.0,  # sum of weight column - optional
-
-}
-
-
-# add invioce addresses? -
-selected_columns = ["Plant Name",
-                    "Sales Order Nbr",
-                    "Incoterms",
-                    "10-Digit UK Import",
-                    "Number Pieces",
-                    "Qty Shipped Net Weight KG",
-                    "Customs Value",
-                    "Currency",
-                    "Country Of Origin",
-                    "General description",
-                    "Date Creation Record",
-                    "EORI Nbr"]
-
-
-# create table here
-# can this be added in the BIS server instead? I can't see anything in the CCT Json that allows
-# item level addresses
-address_data_table = {
-    "Blois": ["FRHBFBLO01", "HB FULLER", "ALLEE ROBERT SCHUMAN C.S 1308", "BLOIS", "41013", "FR"],
-    "Lueneburg": ["DEHBFLUE01", "H.B. Fuller Deutschland GmbH", "An der Roten Bleiche 2-3", "Lueneburg", "21335", "DE"],
-    "Pianezze": ["ITHBFPIA01", "HB Fuller", "VIA DEL INDUSTRIA 8", "PIANEZZE", "36060", "IT"],
-    "Nienburg": ["DEHBFNIE01", "HB FULLER DEUTSCHLAND PRODUKTIONS", "HENRIETTENSTRASSE 32", "NIENBURG", "31582", "DE"],
-    "Mindelo": ["PTHBFMIN01", "HB FULLER", "Estrada Nacional 13/km16", "Mindelo", "4486-851", "PT"],
-}
-
-
-def add_address_data(row):
-    # check if address is in table
-    # "Plant Name"
-    address_rows = {}
-    plant_names = ["Blois", "Lueneburg", "Pianezze", "Nienburg", "Mindelo"]
-
-    for name in plant_names:
-        if name in row["Plant Name"]:
-            data = address_data_table[name]
-
-            address_rows = {
-                "Exporter Name": data[1],
-                "Exporter Street":  data[2],
-                "Exporter City": data[3],
-                "Exporter PostCode": data[4],
-                "Exporter Country": data[5],
-                "Exporter ShortCode": data[0],
-                "Dispatch Country": data[5],
-                "Destination Country": 'GB',
-            }
-
-    return address_rows
-
-
-def get_worksheets(input_file):
-    excel_file = pd.ExcelFile(input_file)
-    return excel_file.sheet_names
-
-
-def read_excel(file_path, sheet_name=None):
-    df = pd.read_excel(file_path, sheet_name=sheet_name)
-    return df
-
-# is there any point of doing this?
-
-# rename dataframe column names to CCT names
-
-# check delivery terms
-
-
-def process_excel(df):
-    # rename columns to json schema headers
-    # preprocess
-
-    # "sequentialNo_SAD32": 6,
-    # df.index = df.index.rename('sequentialNo_SAD32')
-    # Create the new column
-    df.insert(0, 'sequentialNo_SAD32', range(1, len(df) + 1))
-
-    df.rename(columns={"Currency": "goodsValueCurrency"}, inplace=True)
-    df.rename(
-        columns={"General description": "goodsDescription_SAD31"}, inplace=True)
-    df.rename(
-        columns={"Qty Shipped Net Weight KG": "netMass_SAD38"}, inplace=True)
-
-    # copy net weight to gross weight column
-    df.rename(
-        columns={"Qty Shipped Net Weight KG": "netMass_SAD38"}, inplace=True)
-
-    #  grossMass_SAD35
-    df.rename(
-        columns={"General description": "goodsDescription_SAD31"}, inplace=True)
-    df.rename(columns={"Number Pieces": "noOfPackages_SAD31"}, inplace=True)
-    df.rename(
-        columns={"10-Digit UK Import": "customerHSCode_SAD33im"}, inplace=True)
-    df.rename(columns={"Customs Value": "itemPrice_SAD42"}, inplace=True)
-    df.rename(
-        columns={"Customs Value": "itemPrice_SAD42"}, inplace=True)
-    df.rename(
-        columns={"Country Of Origin": "countryOfOrigin_SAD34"}, inplace=True)
-    df.rename(
-        columns={"Sales Order Nbr": "invoiceNo"}, inplace=True)
-    df['grossMass_SAD35'] = df['netMass_SAD38']
-
-    # "Date Creation Record" - ???
-    return df
-
-
-# Add to invoice addresses
-# for each line in the address, create a node in invoice addresses
-
-def add_address_data(row):
-    # check if address is in table
-    # "Plant Name"
-    address_rows = {}
-    plant_names = ["Blois", "Lueneburg", "Pianezze", "Nienburg", "Mindelo"]
-
-    for name in plant_names:
-        if name in row["Plant Name"]:
-            data = address_data_table[name]
-
-            address_rows = {
-                "Exporter Name": data[1],
-                "Exporter Street":  data[2],
-                "Exporter City": data[3],
-                "Exporter PostCode": data[4],
-                "Exporter Country": data[5],
-                "Exporter ShortCode": data[0],
-                "Dispatch Country": data[5],
-                "Destination Country": 'GB',
-            }
-
-    return address_rows
-
-
-# TODO:
-
-# scan through Delivery terms data -
-# create options based on the delivery terms for HBF -
-# this will be in the HBF pre-processor
-
-# Initialize a CustomsOrder
-customs_order = {
-    "addresses": [],
-    "positions": [],
-    "invoices": [],
-}
-
-# format CustomsOrder{
-# "addresses": [
-# ],
-# "positions": [
-# ]
-# "invoices": [
-# ]
-#
-#
-# }
-# convert data types
-# add header data
-# add address data
-# add invoices
-# format quantities to not be decimals
-
-# I don't want to do too much in my own script, I want to s
-
-
-def add_header_data():
-    pass
-
-
-def convert_df_to_json(df):
-    # df.to_json()
-    df.to_json('output.json', orient='records')
-
-    #
-
-
-# TODO: come back and amend this
-# this will just have to match the 912 Reference, to be added as the importer
-# setup in CCT
-hbf_customers = [
-    'HBF912UK (912)',
-    'HBF914UK (914)'
-]
-
-
-def main(input_file):
-    menu = get_worksheets(input_file)
-    questions = [
-        {
-            'type': 'list',
-            'name': 'sheet',
-            'message': 'Select a sheet:',
-            'choices': menu
-        },
-        {
-            'type': 'input',
-            'name': 'valid_row_threshold',
-            'message': 'Enter the minimum number of non-null values in a row:',
-            'validate': lambda val: val.isdigit() and int(val) > 0,
-            'filter': lambda val: int(val)
-        },
-        {
-            'type': 'input',
-            'name': 'user_input',
-            'message': 'Please enter Reference [box 7]:'
-        },
-        {
-            'type': 'list',
-            'name': 'customer_list',
-            'message': 'Select a customer',
-            'choices': hbf_customers
-        }
-    ]
-
-    answers = prompt(questions)
-    df = read_excel(input_file, answers['sheet'])
-    df_remove_null = df.dropna(thresh=answers['valid_row_threshold'])
-    additional_header_data['customerReference'] = answers['user_input']
-
-    # FIXME: pass this data in from a separate script
-    additional_header_data['customerReference'] = answers['user_input']
-    additional_header_data['customerIdCCT'] = answers['customer_list']
-
-    incoterms = df['Incoterms'].unique().tolist()
-
-    # if there is more than one set of incoterms -
-
-    questions = [
-        {
-            'type': 'list',
-            'name': 'incoterm',
-            'message': 'Select an incoterm:',
-            'choices': incoterms
-        }
-    ]
-
-    answers = prompt(questions)
-
-    print('Selected incoterm:', answers['incoterm'])
-
-    # if "incoterms" exists in dataset, then look at the list
-    # processing...
-    # prompt - analyse the incoterm
-    # week 18, week 19 etc.
-
-    # if there are multiple eori numbers in the dataset - prompt the user
-
-    # if df['incoterm']
-
-    # print(df.head())  # Now this line will print to the terminal normally
-    df_subset = df_remove_null[selected_columns].copy()
-
-    processed_df = process_excel(df_subset)
-
-    print(df_subset)
-
-    print(f"Succesfully processed json file")
-    # output json file in json structure
-
-    # do you want to transmit to CCT?
-
-    convert_df_to_json(processed_df)
-
-    # process the file here
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("format: python hb_fuller_preprocessor <excel workbook>")
-        sys.exit(1)
-
-    main(sys.argv[1])
+# from prompt_toolkit.shortcuts import get_input
+
+    # selected_incoterms = []
+    # for i, term in enumerate(incoterms, start=1):
+    #     print(f"{i}. {term}")
+
+    # while True:
+    #     for i, term in enumerate(incoterms, start=1):
+    #         print(f"{i}. {term}")
+    #         # print("Selected incoterms: ", selected_incoterms)
+
+    #     choice = get_input(
+    #         "Select an incoterm (by number), type 'q' to finish, or 's' to show selected: ")
+    #     print('Selected incoterms: ', selected_incoterms)
+    #     # if choice.lower() == 'q':
+    #     #     break
+    #     # elif choice.lower() == 's':
+    #     #     print("Selected incoterms: ", selected_incoterms)
+    #     #     continue
+
+    #     try:
+    #         selected_incoterms.append(incoterms[int(choice) - 1])
+    #         print("Added: ", incoterms[int(choice) - 1])
+    #     except (ValueError, IndexError):
+    #         print("Invalid choice, please try again.")
+
+    #     print("Selected incoterms: ", selected_incoterms)
+
+    # select by week
+    # Result is a list of selected items
+    # print(result)
+
+    # questions = [
+    #     {
+    #         'type': 'checkbox',
+    #         'name': 'incoterms',
+    #         'message': 'Select incoterms:',
+    #         'choices': [{'name': incoterm} for incoterm in incoterms],
+    #         'validate': lambda answer: 'You must select at least one incoterm.' if len(answer) == 0 else True
+    #     }
+    # ]
+
+    # answers = prompt(questions)
+
+    # print('Selected incoterms:', ', '.join(answers['incoterms']))
